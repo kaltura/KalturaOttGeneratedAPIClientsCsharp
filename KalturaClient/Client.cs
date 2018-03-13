baf3b14aceede3094921d39be46eb8d2dcb0ef17
Jenkins <jenkins@kaltura.com>
2018-03-13 14:08:24 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2017  Kaltura Inc.
+// Copyright (C) 2006-2018  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "4.6.35.12397";
-				ClientTag = "dotnet:18-01-24";
+				ApiVersion = "4.6.38.25393";
+				ClientTag = "dotnet:18-03-13";
 		}
 	
 		#region Properties
