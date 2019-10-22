ae0f2a28082d455ac6686b0742c9433af437da32
Backend CI <ott.rnd.core@kaltura.com>
2019-10-22 12:12:18 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "5.2.6.13650";
-				ClientTag = "dotnet:19-10-20";
+				ApiVersion = "5.2.6.6036";
+				ClientTag = "dotnet:19-10-22";
 		}
 	
 		#region Properties
