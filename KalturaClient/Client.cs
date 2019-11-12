aafa0dde48edad8d1d564fb1257ffb048913ee46
Backend CI <ott.rnd.core@kaltura.com>
2019-11-12 18:50:42 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,7 +35,7 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "5.2.6.6080";
+				ApiVersion = "5.2.6.6081";
 				ClientTag = "dotnet:19-11-12";
 		}
 	
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Kaltura C# OTT API Client Library.
-Compatible with Kaltura OTT server version 5.2.6.6080 and above.
+Compatible with Kaltura OTT server version 5.2.6.6081 and above.
