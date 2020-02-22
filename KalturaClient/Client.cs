85be72c444a969f180a002ba4a658ecc3ba57d24
Backend CI <ott.rnd.core@kaltura.com>
2020-02-22 22:33:12 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2626, branch 5_3_2

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "5.3.2.14724";
-				ClientTag = "dotnet:20-02-21";
+				ApiVersion = "5.3.2.14725";
+				ClientTag = "dotnet:20-02-22";
 		}
 	
 		#region Properties
