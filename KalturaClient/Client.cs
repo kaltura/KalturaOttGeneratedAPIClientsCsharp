3f486c9bddec8858d9bf26aac92e073995645a26
Backend CI <ott.rnd.core@kaltura.com>
2020-08-05 13:26:30 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/132, branch 550_BEO-8626_configurable_partial_billing_for_unified_billing

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "5.4.0.28257";
-				ClientTag = "dotnet:20-07-30";
+				ApiVersion = "5.4.0.28245";
+				ClientTag = "dotnet:20-08-05";
 		}
 	
 		#region Properties
