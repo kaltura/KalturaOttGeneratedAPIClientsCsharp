c5c07189c378145da783c36998934dd487d95905
Backend CI <ott.rnd.core@kaltura.com>
2021-07-15 08:18:25 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/545, branch 661_BEO-10239_Device_Mobility_Policy

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,7 +35,7 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "6.5.0.29190";
+				ApiVersion = "6.6.0.29206";
 				ClientTag = "dotnet:21-07-15";
 		}
 	
