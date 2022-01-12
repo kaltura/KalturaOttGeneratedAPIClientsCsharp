cc10a4baad5241764652161a98325b0af37cb379
Backend CI <ott.rnd.core@kaltura.com>
2022-01-12 18:21:43 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/752, branch 7_1_1

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2021  Kaltura Inc.
+// Copyright (C) 2006-2022  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "7.1.0.29666";
-				ClientTag = "dotnet:21-12-23";
+				ApiVersion = "7.1.0.29667";
+				ClientTag = "dotnet:22-01-12";
 		}
 	
 		#region Properties
