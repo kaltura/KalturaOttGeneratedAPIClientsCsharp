8587cf47f0bf2c96abd4bf25eef3e18ec96997c2
Backend CI <ott.rnd.core@kaltura.com>
2022-09-15 08:13:39 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/966, branch 8_0_1

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "7.9.0.30000";
-				ClientTag = "dotnet:22-08-23";
+				ApiVersion = "8.0.0.30024";
+				ClientTag = "dotnet:22-09-15";
 		}
 	
 		#region Properties
--- KalturaClient/Types/IotClientConfiguration.cs
+++ KalturaClient/Types/IotClientConfiguration.cs
@@ -66,7 +66,7 @@ namespace Kaltura.Types
 		private string _ThingId = null;
 		private string _Username = null;
 		private string _Password = null;
-		private IList<StringValue> _Topics;
+		private string _Topics = null;
 		private string _Status = null;
 		private string _Message = null;
 		#endregion
@@ -219,7 +219,7 @@ namespace Kaltura.Types
 		/// Use TopicsAsDouble property instead
 		/// </summary>
 		[JsonProperty]
-		public IList<StringValue> Topics
+		public string Topics
 		{
 			get { return _Topics; }
 			set 
@@ -309,11 +309,7 @@ namespace Kaltura.Types
 			}
 			if(node["topics"] != null)
 			{
-				this._Topics = new List<StringValue>();
-				foreach(var arrayNode in node["topics"].Children())
-				{
-					this._Topics.Add(ObjectFactory.Create<StringValue>(arrayNode));
-				}
+				this._Topics = node["topics"].Value<string>();
 			}
 			if(node["status"] != null)
 			{
