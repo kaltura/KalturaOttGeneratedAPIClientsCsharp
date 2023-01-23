cbb1e0f613d4c4374c372922115990f135735bd7
Backend CI <ott.rnd.core@kaltura.com>
2023-01-23 14:48:49 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1037, branch 850_BEO-13343_filter_by_dependency_type

--- KalturaClient/ObjectFactory.cs
+++ KalturaClient/ObjectFactory.cs
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2022  Kaltura Inc.
+// Copyright (C) 2006-2023  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -140,10 +140,10 @@ namespace Kaltura
 					return new ListResponse<EngagementAdapter>(jToken);
 				case "KalturaEngagementListResponse":
 					return new ListResponse<Engagement>(jToken);
-				case "KalturaInboxMessageListResponse":
-					return new ListResponse<InboxMessage>(jToken);
 				case "KalturaFollowTvSeriesListResponse":
 					return new ListResponse<FollowTvSeries>(jToken);
+				case "KalturaInboxMessageListResponse":
+					return new ListResponse<InboxMessage>(jToken);
 				case "KalturaPersonalFeedListResponse":
 					return new ListResponse<PersonalFeed>(jToken);
 				case "KalturaReminderListResponse":
