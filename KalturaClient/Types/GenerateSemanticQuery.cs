d55ad4d48bbd844ff5dbd9d9b63947df9690b406
Backend CI <ott.rnd.core@kaltura.com>
2025-04-24 13:23:45 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1460, branch BEO-17082-BEO-17133

---
+++ KalturaClient/Types/GenerateSemanticQuery.cs
@@ -0,0 +1,100 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platforms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2023  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+using System;
+using System.Xml;
+using System.Collections.Generic;
+using Kaltura.Enums;
+using Kaltura.Request;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Kaltura.Types
+{
+	public class GenerateSemanticQuery : ObjectBase
+	{
+		#region Constants
+		public const string QUERY = "query";
+		#endregion
+
+		#region Private Fields
+		private string _Query = null;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Use QueryAsDouble property instead
+		/// </summary>
+		[JsonProperty]
+		public string Query
+		{
+			get { return _Query; }
+			set 
+			{ 
+				_Query = value;
+				OnPropertyChanged("Query");
+			}
+		}
+		#endregion
+
+		#region CTor
+		public GenerateSemanticQuery()
+		{
+		}
+
+		public GenerateSemanticQuery(JToken node) : base(node)
+		{
+			if(node["query"] != null)
+			{
+				this._Query = node["query"].Value<string>();
+			}
+		}
+		#endregion
+
+		#region Methods
+		public override Params ToParams(bool includeObjectType = true)
+		{
+			Params kparams = base.ToParams(includeObjectType);
+			if (includeObjectType)
+				kparams.AddReplace("objectType", "KalturaGenerateSemanticQuery");
+			kparams.AddIfNotNull("query", this._Query);
+			return kparams;
+		}
+		protected override string getPropertyName(string apiName)
+		{
+			switch(apiName)
+			{
+				case QUERY:
+					return "Query";
+				default:
+					return base.getPropertyName(apiName);
+			}
+		}
+		#endregion
+	}
+}
+
