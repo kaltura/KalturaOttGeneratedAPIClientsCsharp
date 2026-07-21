507e165b7d2fdbb94a898641beb8ffe4721cb02c
Backend CI <ott.rnd.core@kaltura.com>
2026-07-21 12:38:02 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1635, branch 12.6.0.3

--- KalturaClient/Types/BaseAttributeConstraint.cs
+++ KalturaClient/Types/BaseAttributeConstraint.cs
@@ -35,73 +35,42 @@ using Newtonsoft.Json.Linq;
 
 namespace Kaltura.Types
 {
-	public class AiMetadataGeneratorConfiguration : ObjectBase
+	public class BaseAttributeConstraint : ObjectBase
 	{
 		#region Constants
-		public const string ASSET_STRUCT_CONFIG_MAP = "assetStructConfigMap";
-		public const string SUPPORTED_LANGUAGES = "supportedLanguages";
+		public const string KEY = "key";
 		#endregion
 
 		#region Private Fields
-		private IDictionary<string, MetadataFieldConfigurationMap> _AssetStructConfigMap;
-		private IList<StringValue> _SupportedLanguages;
+		private string _Key = null;
 		#endregion
 
 		#region Properties
 		/// <summary>
-		/// Use AssetStructConfigMapAsDouble property instead
+		/// Use KeyAsDouble property instead
 		/// </summary>
 		[JsonProperty]
-		public IDictionary<string, MetadataFieldConfigurationMap> AssetStructConfigMap
+		public string Key
 		{
-			get { return _AssetStructConfigMap; }
+			get { return _Key; }
 			set 
 			{ 
-				_AssetStructConfigMap = value;
-				OnPropertyChanged("AssetStructConfigMap");
-			}
-		}
-		/// <summary>
-		/// Use SupportedLanguagesAsDouble property instead
-		/// </summary>
-		[JsonProperty]
-		public IList<StringValue> SupportedLanguages
-		{
-			get { return _SupportedLanguages; }
-			private set 
-			{ 
-				_SupportedLanguages = value;
-				OnPropertyChanged("SupportedLanguages");
+				_Key = value;
+				OnPropertyChanged("Key");
 			}
 		}
 		#endregion
 
 		#region CTor
-		public AiMetadataGeneratorConfiguration()
+		public BaseAttributeConstraint()
 		{
 		}
 
-		public AiMetadataGeneratorConfiguration(JToken node) : base(node)
+		public BaseAttributeConstraint(JToken node) : base(node)
 		{
-			if(node["assetStructConfigMap"] != null)
+			if(node["key"] != null)
 			{
-				{
-					string key;
-					this._AssetStructConfigMap = new Dictionary<string, MetadataFieldConfigurationMap>();
-					foreach(var arrayNode in node["assetStructConfigMap"].Children<JProperty>())
-					{
-						key = arrayNode.Name;
-						this._AssetStructConfigMap[key] = ObjectFactory.Create<MetadataFieldConfigurationMap>(arrayNode.Value);
-					}
-				}
-			}
-			if(node["supportedLanguages"] != null)
-			{
-				this._SupportedLanguages = new List<StringValue>();
-				foreach(var arrayNode in node["supportedLanguages"].Children())
-				{
-					this._SupportedLanguages.Add(ObjectFactory.Create<StringValue>(arrayNode));
-				}
+				this._Key = node["key"].Value<string>();
 			}
 		}
 		#endregion
@@ -111,19 +80,16 @@ namespace Kaltura.Types
 		{
 			Params kparams = base.ToParams(includeObjectType);
 			if (includeObjectType)
-				kparams.AddReplace("objectType", "KalturaAiMetadataGeneratorConfiguration");
-			kparams.AddIfNotNull("assetStructConfigMap", this._AssetStructConfigMap);
-			kparams.AddIfNotNull("supportedLanguages", this._SupportedLanguages);
+				kparams.AddReplace("objectType", "KalturaBaseAttributeConstraint");
+			kparams.AddIfNotNull("key", this._Key);
 			return kparams;
 		}
 		protected override string getPropertyName(string apiName)
 		{
 			switch(apiName)
 			{
-				case ASSET_STRUCT_CONFIG_MAP:
-					return "AssetStructConfigMap";
-				case SUPPORTED_LANGUAGES:
-					return "SupportedLanguages";
+				case KEY:
+					return "Key";
 				default:
 					return base.getPropertyName(apiName);
 			}
--- KalturaClient/Types/ContentTypeSelector.cs
+++ KalturaClient/Types/ContentTypeSelector.cs
@@ -35,73 +35,80 @@ using Newtonsoft.Json.Linq;
 
 namespace Kaltura.Types
 {
-	public class AiMetadataGeneratorConfiguration : ObjectBase
+	public class ContentTypeSelector : ObjectBase
 	{
 		#region Constants
-		public const string ASSET_STRUCT_CONFIG_MAP = "assetStructConfigMap";
-		public const string SUPPORTED_LANGUAGES = "supportedLanguages";
+		public const string INCLUDE_RECORDINGS = "includeRecordings";
+		public const string INCLUDE_PROGRAMS = "includePrograms";
+		public const string MEDIA_TYPE_ID_IN = "mediaTypeIdIn";
 		#endregion
 
 		#region Private Fields
-		private IDictionary<string, MetadataFieldConfigurationMap> _AssetStructConfigMap;
-		private IList<StringValue> _SupportedLanguages;
+		private bool? _IncludeRecordings = null;
+		private bool? _IncludePrograms = null;
+		private string _MediaTypeIdIn = null;
 		#endregion
 
 		#region Properties
 		/// <summary>
-		/// Use AssetStructConfigMapAsDouble property instead
+		/// Use IncludeRecordingsAsDouble property instead
 		/// </summary>
 		[JsonProperty]
-		public IDictionary<string, MetadataFieldConfigurationMap> AssetStructConfigMap
+		public bool? IncludeRecordings
 		{
-			get { return _AssetStructConfigMap; }
+			get { return _IncludeRecordings; }
 			set 
 			{ 
-				_AssetStructConfigMap = value;
-				OnPropertyChanged("AssetStructConfigMap");
+				_IncludeRecordings = value;
+				OnPropertyChanged("IncludeRecordings");
 			}
 		}
 		/// <summary>
-		/// Use SupportedLanguagesAsDouble property instead
+		/// Use IncludeProgramsAsDouble property instead
 		/// </summary>
 		[JsonProperty]
-		public IList<StringValue> SupportedLanguages
+		public bool? IncludePrograms
 		{
-			get { return _SupportedLanguages; }
-			private set 
+			get { return _IncludePrograms; }
+			set 
 			{ 
-				_SupportedLanguages = value;
-				OnPropertyChanged("SupportedLanguages");
+				_IncludePrograms = value;
+				OnPropertyChanged("IncludePrograms");
+			}
+		}
+		/// <summary>
+		/// Use MediaTypeIdInAsDouble property instead
+		/// </summary>
+		[JsonProperty]
+		public string MediaTypeIdIn
+		{
+			get { return _MediaTypeIdIn; }
+			set 
+			{ 
+				_MediaTypeIdIn = value;
+				OnPropertyChanged("MediaTypeIdIn");
 			}
 		}
 		#endregion
 
 		#region CTor
-		public AiMetadataGeneratorConfiguration()
+		public ContentTypeSelector()
 		{
 		}
 
-		public AiMetadataGeneratorConfiguration(JToken node) : base(node)
+		public ContentTypeSelector(JToken node) : base(node)
 		{
-			if(node["assetStructConfigMap"] != null)
+			if(node["includeRecordings"] != null)
 			{
-				{
-					string key;
-					this._AssetStructConfigMap = new Dictionary<string, MetadataFieldConfigurationMap>();
-					foreach(var arrayNode in node["assetStructConfigMap"].Children<JProperty>())
-					{
-						key = arrayNode.Name;
-						this._AssetStructConfigMap[key] = ObjectFactory.Create<MetadataFieldConfigurationMap>(arrayNode.Value);
-					}
-				}
+				this._IncludeRecordings = ParseBool(node["includeRecordings"].Value<string>());
 			}
-			if(node["supportedLanguages"] != null)
+			if(node["includePrograms"] != null)
 			{
-				this._SupportedLanguages = new List<StringValue>();
-				foreach(var arrayNode in node["supportedLanguages"].Children())
-				{
-					this._SupportedLanguages.Add(ObjectFactory.Create<StringValue>(arrayNode));
-				}
+				this._IncludePrograms = ParseBool(node["includePrograms"].Value<string>());
+			}
+			if(node["mediaTypeIdIn"] != null)
+			{
+				this._MediaTypeIdIn = node["mediaTypeIdIn"].Value<string>();
 			}
 		}
 		#endregion
@@ -111,19 +118,22 @@ namespace Kaltura.Types
 		{
 			Params kparams = base.ToParams(includeObjectType);
 			if (includeObjectType)
-				kparams.AddReplace("objectType", "KalturaAiMetadataGeneratorConfiguration");
-			kparams.AddIfNotNull("assetStructConfigMap", this._AssetStructConfigMap);
-			kparams.AddIfNotNull("supportedLanguages", this._SupportedLanguages);
+				kparams.AddReplace("objectType", "KalturaContentTypeSelector");
+			kparams.AddIfNotNull("includeRecordings", this._IncludeRecordings);
+			kparams.AddIfNotNull("includePrograms", this._IncludePrograms);
+			kparams.AddIfNotNull("mediaTypeIdIn", this._MediaTypeIdIn);
 			return kparams;
 		}
 		protected override string getPropertyName(string apiName)
 		{
 			switch(apiName)
 			{
-				case ASSET_STRUCT_CONFIG_MAP:
-					return "AssetStructConfigMap";
-				case SUPPORTED_LANGUAGES:
-					return "SupportedLanguages";
+				case INCLUDE_RECORDINGS:
+					return "IncludeRecordings";
+				case INCLUDE_PROGRAMS:
+					return "IncludePrograms";
+				case MEDIA_TYPE_ID_IN:
+					return "MediaTypeIdIn";
 				default:
 					return base.getPropertyName(apiName);
 			}
